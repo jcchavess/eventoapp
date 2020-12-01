6624031745a39c4582bca142349deef22da1f83a
jcchavess <55768926+jcchavess@users.noreply.github.com>
2020-11-30 23:30:40 -0300

    Primeiro Commit
    Jaozin

---
+++ src/main/java/com/eventoapp/eventoapp/models/Convidado.java
@@ -0,0 +1,44 @@
+package com.eventoapp.eventoapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Convidado{
+
+    @Id
+    @NotEmpty
+    private String cpf;
+
+    @NotEmpty
+    private String nomeConvidado;
+
+    @ManyToOne
+    private Evento evento;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNomeConvidado() {
+        return nomeConvidado;
+    }
+
+    public void setNomeConvidado(String nomeConvidado) {
+        this.nomeConvidado = nomeConvidado;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+}
